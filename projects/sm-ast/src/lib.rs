7cf64f7deb2d7ad129a197d319230f6dbda7cece
Aster <galaster@foxmail.com>
2020-06-12 19:58:08 +0800

    🚀 Build wasm for github pages

--- projects/sm-ast/src/lib.rs
+++ projects/sm-ast/src/lib.rs
@@ -1,16 +1,19 @@
 #![feature(option_unwrap_none)]
 
-pub mod ast;
+mod ast;
 mod error;
 pub mod evaluate;
+
 #[allow(dead_code)]
-mod internal;
-pub mod parser;
+pub mod internal;
 pub mod traits;
+pub mod parser;
 
 pub use ast::AST;
 pub use error::{SMError, SMResult};
 pub use traits::{ToTex, ToWolfram};
+pub use evaluate::{Runner,Context};
+
 
 #[test]
 fn it_works() {}
